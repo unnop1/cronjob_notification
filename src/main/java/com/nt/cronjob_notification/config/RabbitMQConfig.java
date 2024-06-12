8d67a2bfcff6716e7340904bbc274046954c373a
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-06-12 15:54:12 +0700

    test deploy

--- src/main/java/com/nt/cronjob_notification/config/RabbitMQConfig.java
+++ src/main/java/com/nt/cronjob_notification/config/RabbitMQConfig.java
@@ -5,12 +5,13 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class RabbitMQConfig {
 
-    @Value("${spring.rabbitmq.addresses}")
-    private String rabbitAddresses;
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitHost;
 
     @Value("${spring.rabbitmq.username}")
     private String rabbitUsername;
@@ -24,7 +25,12 @@ public class RabbitMQConfig {
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setAddresses(rabbitAddresses);
+        
+        if (StringUtils.hasText(rabbitHost)) {
+            connectionFactory.setHost(rabbitHost);
+            connectionFactory.setPort(5672); // Default RabbitMQ port
+        }
+
         connectionFactory.setUsername(rabbitUsername);
         connectionFactory.setPassword(rabbitPassword);
         connectionFactory.setVirtualHost(rabbitVirtualHost);
